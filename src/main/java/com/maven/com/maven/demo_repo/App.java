3b8b5faea062f64cb00fb198d97e57fb7747a607
Coder293 <72380124+Coder293@users.noreply.github.com>
2021-06-29 23:35:46 +0200

    Add files via upload

---
+++ src/main/java/com/maven/com/maven/demo_repo/App.java
@@ -0,0 +1,13 @@
+package com.maven.com.maven.demo_repo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
